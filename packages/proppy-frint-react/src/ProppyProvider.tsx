f80b07190e9d7cf8c598e7d555207a0176654205
Ryan <ryan.c.edge@outlook.com>
2018-07-25 13:26:16 -0400

    Removed babel & eslint
    Both references and configs

--- packages/proppy-frint-react/src/ProppyProvider.tsx
+++ packages/proppy-frint-react/src/ProppyProvider.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react'; // eslint-disable-line
+import * as React from 'react';
 import { observe, streamProps, ProviderProps } from 'frint-react';
-import { ProppyProvider as ProppyReactProvider } from 'proppy-react'; // eslint-disable-line
+import { ProppyProvider as ProppyReactProvider } from 'proppy-react';
 
 export class Base extends React.Component<ProviderProps & { providers }> {
   public render() {
