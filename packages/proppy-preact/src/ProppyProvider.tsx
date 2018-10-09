f143240117278a3088295dd0a3647335c4cf7ae9
David Konsumer <konsumer@jetboystudio.com>
2018-10-08 17:08:30 -0700

    should resolve #35
    use ProppyContext's Provider for ProppyProvider

--- packages/proppy-preact/src/ProppyProvider.tsx
+++ packages/proppy-preact/src/ProppyProvider.tsx
@@ -1,16 +1,3 @@
-import { Component } from 'preact';
-
-/**
- * Source: https://gist.github.com/developit/5d879edb820228224dc9
- */
-export class ProppyProvider extends Component {
-  public getChildContext() {
-    const { children, ...context } = this.props;
-
-    return context;
-  }
-
-  public render({ children }) {
-    return (children && children[0]) || null;
-  }
-}
+import { ProppyContext } from './ProppyContext';
+const ProppyProvider = ProppyContext.Provider;
+export ProppyProvider;
