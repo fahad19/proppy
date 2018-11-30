18ab63b66cc13be4d815541a540f9ddf6de77915
Fahad Ibnay Heylaal <fahad19@gmail.com>
2018-11-30 13:38:54 +0100

    proppy-react-hook: hook in progress.

--- packages/proppy-react-hook/src/useProppy.ts
+++ packages/proppy-react-hook/src/useProppy.ts
@@ -1,4 +1,21 @@
-export function useProppy() {
-  // @TODO
-  return true;
+import React, { useContext, useEffect, useState } from 'react';
+import { ProppyContext } from 'proppy-react';
+
+export function useProppy(P, props = {}) {
+  const providers = useContext(ProppyContext);
+  const p = P(props, providers);
+  const [proppyState, setProppyState] = useState(p.props);
+
+  useEffect(() => {
+    const unsubscribe = p.subscribe(
+      newProps => setProppyState(newProps)
+    );
+
+    return () => {
+      unsubscribe();
+      p.destroy();
+    };
+  });
+
+  return proppyState;
 }
