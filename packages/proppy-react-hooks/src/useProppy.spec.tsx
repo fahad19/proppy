bff837eb4029137ea7f17590724bb6e955e37a6b
Fahad Ibnay Heylaal <fahad19@gmail.com>
2018-12-11 21:44:15 +0100

    tests not running when more than one render involved.

--- packages/proppy-react-hooks/src/useProppy.spec.tsx
+++ packages/proppy-react-hooks/src/useProppy.spec.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import { compose, withProps } from 'proppy';
+import { compose, withProps, withState } from 'proppy';
 import { ProppyProvider, attach } from 'proppy-react';
 
 import { useProppy } from './useProppy';
@@ -11,8 +11,14 @@ describe('proppy-react-hooks :: useProppy', () => {
   let reactRoot;
 
   beforeEach(() => {
+    resetDOM();
+
     reactRoot = document.createElement('div');
     document.body.appendChild(reactRoot);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(reactRoot);
   });
 
   test('is a function', () => {
@@ -27,7 +33,7 @@ describe('proppy-react-hooks :: useProppy', () => {
     );
 
     function Child(props) {
-      const { foo } = useProppy(P, props);
+      const { foo } = useProppy(P);
 
       return <p>{foo}</p>;
     }
@@ -43,4 +49,56 @@ describe('proppy-react-hooks :: useProppy', () => {
     ReactDOM.render(<Root />, reactRoot);
     expect(reactRoot.textContent).toEqual('foo value');
   });
+
+  test('----sync single render', () => {
+    const providers = {};
+
+    const P = compose(
+      withProps({ foo: 'foo value' })
+    );
+
+    function Child(props) {
+      const { foo } = useProppy(P);
+
+      return <p>{foo}</p>;
+    }
+
+    function Root() {
+      return (
+        <ProppyProvider providers={providers}>
+          <Child />
+        </ProppyProvider>
+      );
+    }
+
+    ReactDOM.render(<Root />, reactRoot);
+    expect(reactRoot.textContent).toEqual('foo value');
+  });
+
+  // test('sync multiple renders', () => {
+  //   const providers = {};
+
+  //   const P = compose(
+  //     withState('counter', 'setCounter', 0),
+  //   );
+
+  //   function Child(props) {
+  //     const { counter, setCounter } = useProppy(P);
+
+  //     return <p onClick={() => setCounter(counter + 1)}>{counter}</p>;
+  //   }
+
+  //   function Root() {
+  //     return (
+  //       <ProppyProvider providers={providers}>
+  //         <Child />
+  //       </ProppyProvider>
+  //     );
+  //   }
+
+  //   ReactDOM.render(<Root />, reactRoot);
+
+  //   // const p = reactRoot.querySelector('p');
+  //   // console.log(p.textContent);
+  // });
 });
